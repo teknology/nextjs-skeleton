90024885461d5bf33c063f1ee965b4d71e3caf48
Gary Pettigrew <garypettigrew@Garys-Work-MBP.local>
2024-07-24 23:02:04 -0400

    added test form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
+'use client'
 import Image from 'next/image'
 import { GoogleSignIn } from './components/google-sign-in'
 import { auth } from '@/auth'
+import { Button, Input, Text } from '@nextui-org/react'
 
-export default async function Home () {
-  const session = await auth()
+export default function Home () {
+  //const session = await auth()
 
   return (
-    <>
-      <pre>
-        <div>{JSON.stringify(session)}</div>{' '}
-      </pre>
-
-      <GoogleSignIn />
-    </>
+    <form>
+      <Input type='email' placeholder='Email' label='Email' required />
+      <Input type='password' placeholder='Password' label='Password' required />
+      <Button type='submit'>Signin</Button>
+    </form>
   )
 }
