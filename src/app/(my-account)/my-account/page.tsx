038f73831d80721f70000931cf16c06bb4636976
Gary Pettigrew <gary@magehd.com>
2024-07-29 20:25:04 -0400

    Added Marketing card place holder

--- src/app/(my-account)/my-account/page.tsx
+++ src/app/(my-account)/my-account/page.tsx
@@ -30,6 +30,8 @@ import { Icon } from '@iconify/react'
 import * as actions from '@/actions'
 import NotificationsCard from '@/app/components/myaccount/notification-card'
 import { useSession } from 'next-auth/react'
+import MarketingCard from '@/app/components/myaccount/common/marketing-card'
+import TeamMember from '@/app/components/myaccount/team-member'
 
 export default function myAccount () {
   //const session = useSession()
@@ -69,7 +71,38 @@ export default function myAccount () {
         hideScrollBar
         className='mx-2 flex w-full  gap-8'
         orientation='horizontal'
-      ></ScrollShadow>
+      >
+        <div className='flex w-full flex-col'>
+          <Tabs aria-label='Options' color='primary'>
+            <Tab key='account_home' title='Account Home'>
+              {/* TODO: Create an notifications Feature that allows for CTA Cards or informational cards*/}
+              <Card>
+                <CardBody>
+                  <MarketingCard />
+                </CardBody>
+              </Card>
+            </Tab>
+            <Tab key='websites' title='Websites'>
+              <Card>
+                <CardBody>
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur.
+                </CardBody>
+              </Card>
+            </Tab>
+            <Tab key='team' title='Team'>
+              <Card>
+                <CardBody>
+                  {/* Add the feature to invite, add, edit, delete, team members*/}
+                  <TeamMember />
+                </CardBody>
+              </Card>
+            </Tab>
+          </Tabs>
+        </div>
+      </ScrollShadow>
     </>
   )
 }
--- src/app/components/myaccount/common/marketing-card.tsx
+++ src/app/components/myaccount/common/marketing-card.tsx
@@ -3,43 +3,52 @@
 import type { CardProps } from '@nextui-org/react'
 
 import React from 'react'
-import { Button, Card, Image, CardBody } from '@nextui-org/react'
+import {
+  Button,
+  Card,
+  Image,
+  CardBody,
+  CardFooter,
+  CardHeader
+} from '@nextui-org/react'
 import { Icon } from '@iconify/react'
 
 export default function MarketingCard (props: CardProps) {
   return (
-    <Card className='w-full max-w-[520px]' {...props}>
-      <Button
-        isIconOnly
-        className='absolute right-2 top-2 z-20'
-        radius='full'
-        size='sm'
-        variant='light'
-      >
-        <Icon
-          className='text-default-400'
-          icon='iconamoon:close-thin'
-          width={24}
-        />
-      </Button>
-      <CardBody className='flex flex-row flex-wrap p-0 sm:flex-nowrap'>
-        <Image
-          removeWrapper
-          alt='Acme Creators'
-          className='h-auto w-full flex-none object-cover object-top md:w-48'
-          src='https://nextuipro.nyc3.cdn.digitaloceanspaces.com/components-images/hero-card-complete.jpeg'
-        />
-        <div className='px-4 py-5'>
-          <h3 className='text-large font-medium'>Become an Acme Creator!</h3>
-          <div className='flex flex-col gap-3 pt-2 text-small text-default-400'>
-            <p>
-              Visit creators.acme.com to sign up today and start earning credits
-              from your fans and followers.
-            </p>
-            <p>Acme supports YouTube, Twitch, Vimeo and more!</p>
+    <Card
+      isFooterBlurred
+      className='w-full h-[300px] col-span-12 sm:col-span-7'
+    >
+      <CardHeader className='absolute z-10 top-1 flex-col items-start'>
+        <p className='text-tiny text-white/60 uppercase font-bold'>
+          Your day your way
+        </p>
+        <h4 className='text-white/90 font-medium text-xl'>
+          Your checklist for better sleep
+        </h4>
+      </CardHeader>
+      <Image
+        removeWrapper
+        alt='Relaxing app background'
+        className='z-0 w-full h-full object-cover'
+        src='https://nextui.org/images/card-example-5.jpeg'
+      />
+      <CardFooter className='absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100'>
+        <div className='flex flex-grow gap-2 items-center'>
+          <Image
+            alt='Breathing app icon'
+            className='rounded-full w-10 h-11 bg-black'
+            src='https://nextui.org/images/breathing-app-icon.jpeg'
+          />
+          <div className='flex flex-col'>
+            <p className='text-tiny text-white/60'>Breathing App</p>
+            <p className='text-tiny text-white/60'>Get a good night's sleep.</p>
           </div>
         </div>
-      </CardBody>
+        <Button radius='full' size='sm'>
+          Get App
+        </Button>
+      </CardFooter>
     </Card>
   )
 }
