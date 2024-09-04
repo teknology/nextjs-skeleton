ac062212efa097ac94bd14c77a2512dfc7f3c40f
Mage H.D. Inc <magentohd@gmail.com>
2024-09-04 11:24:20 -0400

    Locale data file

---
+++ src/utils/data/languages.ts
@@ -0,0 +1,224 @@
+[
+    {
+        "code": "en",
+        "country": "United States",
+        "language": "English",
+        "flag": "https://flagcdn.com/w320/us.png"
+    },
+    {
+        "code": "fr",
+        "country": "France",
+        "language": "Français",
+        "flag": "https://flagcdn.com/w320/fr.png"
+    },
+    {
+        "code": "es",
+        "country": "Spain",
+        "language": "Español",
+        "flag": "https://flagcdn.com/w320/es.png"
+    },
+    {
+        "code": "de",
+        "country": "Germany",
+        "language": "Deutsch",
+        "flag": "https://flagcdn.com/w320/de.png"
+    },
+    {
+        "code": "jp",
+        "country": "Japan",
+        "language": "日本語",
+        "flag": "https://flagcdn.com/w320/jp.png"
+    },
+    {
+        "code": "cn",
+        "country": "China",
+        "language": "中文",
+        "flag": "https://flagcdn.com/w320/cn.png"
+    },
+    {
+        "code": "kr",
+        "country": "South Korea",
+        "language": "한국어",
+        "flag": "https://flagcdn.com/w320/kr.png"
+    },
+    {
+        "code": "ru",
+        "country": "Russia",
+        "language": "Русский",
+        "flag": "https://flagcdn.com/w320/ru.png"
+    },
+    {
+        "code": "it",
+        "country": "Italy",
+        "language": "Italiano",
+        "flag": "https://flagcdn.com/w320/it.png"
+    },
+    {
+        "code": "pt",
+        "country": "Portugal",
+        "language": "Português",
+        "flag": "https://flagcdn.com/w320/pt.png"
+    },
+    {
+        "code": "ar",
+        "country": "Saudi Arabia",
+        "language": "العربية",
+        "flag": "https://flagcdn.com/w320/sa.png"
+    },
+    {
+        "code": "in",
+        "country": "India",
+        "language": "हिन्दी",
+        "flag": "https://flagcdn.com/w320/in.png"
+    },
+    {
+        "code": "br",
+        "country": "Brazil",
+        "language": "Português (Brasil)",
+        "flag": "https://flagcdn.com/w320/br.png"
+    },
+    {
+        "code": "nl",
+        "country": "Netherlands",
+        "language": "Nederlands",
+        "flag": "https://flagcdn.com/w320/nl.png"
+    },
+    {
+        "code": "se",
+        "country": "Sweden",
+        "language": "Svenska",
+        "flag": "https://flagcdn.com/w320/se.png"
+    },
+    {
+        "code": "no",
+        "country": "Norway",
+        "language": "Norsk",
+        "flag": "https://flagcdn.com/w320/no.png"
+    },
+    {
+        "code": "fi",
+        "country": "Finland",
+        "language": "Suomi",
+        "flag": "https://flagcdn.com/w320/fi.png"
+    },
+    {
+        "code": "dk",
+        "country": "Denmark",
+        "language": "Dansk",
+        "flag": "https://flagcdn.com/w320/dk.png"
+    },
+    {
+        "code": "gr",
+        "country": "Greece",
+        "language": "Ελληνικά",
+        "flag": "https://flagcdn.com/w320/gr.png"
+    },
+    {
+        "code": "tr",
+        "country": "Turkey",
+        "language": "Türkçe",
+        "flag": "https://flagcdn.com/w320/tr.png"
+    },
+    {
+        "code": "pl",
+        "country": "Poland",
+        "language": "Polski",
+        "flag": "https://flagcdn.com/w320/pl.png"
+    },
+    {
+        "code": "cz",
+        "country": "Czech Republic",
+        "language": "Čeština",
+        "flag": "https://flagcdn.com/w320/cz.png"
+    },
+    {
+        "code": "hu",
+        "country": "Hungary",
+        "language": "Magyar",
+        "flag": "https://flagcdn.com/w320/hu.png"
+    },
+    {
+        "code": "ro",
+        "country": "Romania",
+        "language": "Română",
+        "flag": "https://flagcdn.com/w320/ro.png"
+    },
+    {
+        "code": "bg",
+        "country": "Bulgaria",
+        "language": "Български",
+        "flag": "https://flagcdn.com/w320/bg.png"
+    },
+    {
+        "code": "il",
+        "country": "Israel",
+        "language": "עברית",
+        "flag": "https://flagcdn.com/w320/il.png"
+    },
+    {
+        "code": "ir",
+        "country": "Iran",
+        "language": "فارسی",
+        "flag": "https://flagcdn.com/w320/ir.png"
+    },
+    {
+        "code": "th",
+        "country": "Thailand",
+        "language": "ไทย",
+        "flag": "https://flagcdn.com/w320/th.png"
+    },
+    {
+        "code": "vn",
+        "country": "Vietnam",
+        "language": "Tiếng Việt",
+        "flag": "https://flagcdn.com/w320/vn.png"
+    },
+    {
+        "code": "id",
+        "country": "Indonesia",
+        "language": "Bahasa Indonesia",
+        "flag": "https://flagcdn.com/w320/id.png"
+    },
+    {
+        "code": "my",
+        "country": "Malaysia",
+        "language": "Bahasa Melayu",
+        "flag": "https://flagcdn.com/w320/my.png"
+    },
+    {
+        "code": "ph",
+        "country": "Philippines",
+        "language": "Filipino",
+        "flag": "https://flagcdn.com/w320/ph.png"
+    },
+    {
+        "code": "za",
+        "country": "South Africa",
+        "language": "Afrikaans",
+        "flag": "https://flagcdn.com/w320/za.png"
+    },
+    {
+        "code": "eg",
+        "country": "Egypt",
+        "language": "العربية",
+        "flag": "https://flagcdn.com/w320/eg.png"
+    },
+    {
+        "code": "ua",
+        "country": "Ukraine",
+        "language": "Українська",
+        "flag": "https://flagcdn.com/w320/ua.png"
+    },
+    {
+        "code": "et",
+        "country": "Ethiopia",
+        "language": "አማርኛ",
+        "flag": "https://flagcdn.com/w320/et.png"
+    },
+    {
+        "code": "ke",
+        "country": "Kenya",
+        "language": "Kiswahili",
+        "flag": "https://flagcdn.com/w320/ke.png"
+    }
+]
